b58aa33bb5de76d67329e6c956514becf5cb5d84
@decisionnguyen <decisionnguyen@gmail.com>
2020-04-29 14:39:28 +0700

    update package and remove something unuse

--- BaseAuth.ts
+++
@@ -1,5 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { BaseAuth } = NativeModules;
-
-export default BaseAuth;
--- store/rootReducers.ts
+++
@@ -1,7 +0,0 @@
-import authReducer from "./auth/reducers";
-
-const rootReducers = {
-  auth: authReducer
-};
-
-export default rootReducers;
