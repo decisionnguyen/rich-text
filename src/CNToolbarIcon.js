2c13243803e16747c81bc8325a2902d579ef46be
@decisionnguyen <decisionnguyen@gmail.com>
2020-05-21 10:58:46 +0700

    wrap component

--- src/CNToolbarIcon.js
+++ src/CNToolbarIcon.js
@@ -1,8 +1,9 @@
-import React  from 'react'
+import React from 'react';
 import {
     View,
     TouchableWithoutFeedback,
-} from 'react-native'
+    Text,
+} from 'react-native';
 
 export const CNToolbarIcon = (props) => {
     const {
@@ -19,35 +20,43 @@ export const CNToolbarIcon = (props) => {
         buttonTypes,
         selectedBackgroundColor,
     } = props;
-
     let colorCondition = '';
     let backgroundColorCondition = '';
     if (buttonTypes === 'style') {
         backgroundColorCondition = selectedStyles.indexOf(toolTypeText) >= 0 ? selectedBackgroundColor : backgroundColor;
         colorCondition = selectedStyles.indexOf(toolTypeText) >= 0 ? selectedColor : color;
-    }
-    else if (buttonTypes === 'tag') {
+    } else if (buttonTypes === 'tag') {
         backgroundColorCondition = selectedTag === toolTypeText ? selectedBackgroundColor : backgroundColor;
-        colorCondition = selectedTag === toolTypeText ? selectedColor : color
+        colorCondition = selectedTag === toolTypeText ? selectedColor : color;
     }
+    const defaultStyle = typeof iconComponent === Text ? {
+        color: colorCondition,
+        fontSize: size,
+    } : {
+
+    };
+
     return (
         <TouchableWithoutFeedback
             onPress={() => {
-                onStyleKeyPress(toolTypeText)
+                onStyleKeyPress(toolTypeText);
             }}
         >
             <View style={[iconStyles,
                 {
-                    backgroundColor: backgroundColorCondition
+                    backgroundColor: backgroundColorCondition,
                 }]}
             >
                 {
-                   React.cloneElement(iconComponent, { size , color: colorCondition , style: [{
-                    fontSize: size,
-                    color: colorCondition
-                   }, iconComponent.props.style || {}] })
+                    React.cloneElement(iconComponent,
+                        {
+                            size, color: colorCondition,
+                            style: [
+                                defaultStyle,
+                                iconComponent.props.style || {}],
+                        })
                 }
             </View>
         </TouchableWithoutFeedback>
-    )
+    );
 };
