373eec8f7ae34f9d1c0e299e1ac59905ee11f4f8
@decisionnguyen <decisionnguyen@gmail.com>
2020-05-28 11:16:46 +0700

    add scroll bar color

--- src/html/editor.js
+++ src/html/editor.js
@@ -54,6 +54,17 @@ const editorHTML = `
            padding-right: 20px;
         }
 
+          #editor::-webkit-scrollbar {
+            -webkit-appearance: none;
+            width: 3px;
+          }
+          #editor::-webkit-scrollbar-thumb {
+            border-radius: 4px;
+            background-color: rgba(0, 0, 0, .5);
+            -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, .5);
+          }
+        
+
         /*#editor::-webkit-scrollbar {*/
         /*   display: none;*/
         /*}*/
